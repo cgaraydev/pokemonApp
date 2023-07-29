e08469a51203f6b4f2edcd00b8a97f5e1ea4c070
CamiloDev <cgaraydev@gmail.com>
2023-07-29 00:07:33 -0400

    First commit!. Introduction fragment XML layout

---
+++ app/src/main/java/com/example/pokemonapp/activities/PokemonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokemonapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokemonapp.R
+
+class PokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon)
+    }
+}
---
+++ app/src/main/java/com/example/pokemonapp/fragments/loginregister/IntroductionFragment.kt
@@ -0,0 +1,8 @@
+package com.example.pokemonapp.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+
+class IntroductionFragment: Fragment() {
+
+
+}
